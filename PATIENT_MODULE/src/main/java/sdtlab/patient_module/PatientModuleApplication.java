6cf30433be0b170af7a981d30af5c6b73029976f
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2024-12-26 12:49:50 +0200

    PATIENT MODULE: Eureka

--- PATIENT_MODULE/src/main/java/sdtlab/patient_module/PatientModuleApplication.java
+++ PATIENT_MODULE/src/main/java/sdtlab/patient_module/PatientModuleApplication.java
@@ -2,8 +2,10 @@ package sdtlab.patient_module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PatientModuleApplication {
 
     public static void main(String[] args) {
